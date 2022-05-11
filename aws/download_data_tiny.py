0500fced04adaef9b4131675188c0add80e9bc23
Zhenhao <zhenhao.ge@gmail.com>
2022-05-11 05:28:35 +0000

    update run.sh to include downloading data from s3

--- aws/download_data_tiny.py
+++ aws/download_data_tiny.py
@@ -1,9 +1,16 @@
 import os, sys
 import boto3
+import time
+from datetime import timedelta
 
 bucket = sys.argv[1] # e.g. zge-exp (aws:f8dev)
-folder_s3 = sys.argv[2] # e.g. '/data/tiny'
-folder_local = sys.argv[3] # /opt/data/ (docker), '/data6/zge-speechbrain/data/'
+folder_s3 = sys.argv[2] # e.g. 'data/tiny'
+folder_local = sys.argv[3] # /opt/data (docker), '/data6/zge-speechbrain/data'
+
+# # example
+# bucket = 'zge-exp'
+# folder_s3 = 'data/tiny'
+# folder_local = '/home/sagemaker-user/data'
 
 if not os.path.exists(folder_local):
     print('creating dir: {}'.format(folder_local))
@@ -13,6 +20,7 @@ print('{} --> {}'.format(os.path.join(bucket, folder_s3), folder_local))
 
 def download_folder(bucket, folder_s3, folder_local):
     s3 = boto3.resource('s3')
+    start_time = time.monotonic()
     for obj in s3.Bucket(bucket).objects.filter(Prefix=folder_s3):
         infile = obj.key
         pardir = os.path.join(folder_local, os.path.dirname(infile))
@@ -21,7 +29,13 @@ def download_folder(bucket, folder_s3, folder_local):
             os.makedirs(pardir, exist_ok=True)
             print('dir: {} created'.format(pardir))
         outfile = os.path.join(pardir, filename)
-        s3.Bucket(bucket).download_file(infile, outfile)
-        # print('{} --> {} ...'.format(infile, outfile))
+        if os.path.exists(outfile):
+            print('skip {} --> {} ...'.format(infile, outfile))
+        else:
+            s3.Bucket(bucket).download_file(infile, outfile)
+            print('{} --> {} ...'.format(infile, outfile))
+    end_time = time.monotonic()
+    duration = end_time - start_time
+    print('data downloading time: {}'.format(timedelta(seconds=duration))
 
 download_folder(bucket, folder_s3, folder_local)
--- recipes/CommonVoice/run.sh
+++ recipes/CommonVoice/run.sh
@@ -2,4 +2,13 @@
 #
 # run training
 
+
+if aws s3api wait bucket-exists --bucket "zge-exp/" ; then
+  echo "bucket zge-exp exist"
+fi
+
+# download data from s3 bucket
+python ../../aws/download_data_tiny.py zge-exp data/tiny /home/sagemaker-user/data
+
+# run the training
 CUSA_VISIBLE_DEVICES=0,1 python ASR/seq2seq/train_exp_cv_with_ots.py ASR/seq2seq/hparams/train_fr_exp_cv_with_ots_bs2.yml --batch_size=2 --data_parallel_backend
