4377a152284bffec57402410e2cc525ed9a4e2aa
Zhenhao <zhenhao.ge@gmail.com>
2022-05-11 05:35:41 +0000

    update the local folder to switch from f8dev to docker

--- recipes/CommonVoice/run.sh
+++ recipes/CommonVoice/run.sh
@@ -8,7 +8,7 @@ if aws s3api wait bucket-exists --bucket "zge-exp/" ; then
 fi
 
 # download data from s3 bucket
-python ../../aws/download_data_tiny.py zge-exp data/tiny /home/sagemaker-user/data
+python ../../aws/download_data_tiny.py zge-exp data/tiny /opt/data
 
 # run the training
 CUSA_VISIBLE_DEVICES=0,1 python ASR/seq2seq/train_exp_cv_with_ots.py ASR/seq2seq/hparams/train_fr_exp_cv_with_ots_bs2.yml --batch_size=2 --data_parallel_backend
